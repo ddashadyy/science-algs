e2e7b2ee9d5d5ca576ce08c1ae06a8dd32950acf
ddashadyy <poletaevgena75@gmail.com>
2025-04-14 19:26:53 +0800

    cnf generator application

--- src/main.c
+++ src/main.c
@@ -1,24 +1,35 @@
 #define WIN32_LEAN_AND_MEAN
+#define _UNICODE
+#define UNICODE
 
 #include <windows.h>
 #include <stdio.h>
+#include "cnf.h"
+
+
+// Идентификаторы элементов управления
+#define ID_BUTTON 1
+#define ID_EDIT 2
+#define ID_STATIC 3
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-// Точка входа приложения
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) 
 {
 
     _CRT_UNUSED(hPrevInstance);
     _CRT_UNUSED(szCmdLine);
 
-    
-    static TCHAR szAppName[] = TEXT("HelloWin");
+    // Установка русского языка
+    SetThreadLocale(MAKELCID(MAKELANGID(LANG_RUSSIAN, SUBLANG_DEFAULT), SORT_DEFAULT));
+    SetConsoleOutputCP(1251);
+
+    static TCHAR szAppName[] = TEXT("Генератор КНФ");
     HWND hwnd;
     MSG msg;
     WNDCLASS wndclass;
 
-    // Заполняем структуру класса окна
+    // Регистрация класса окна
     wndclass.style = CS_HREDRAW | CS_VREDRAW;
     wndclass.lpfnWndProc = WndProc;
     wndclass.cbClsExtra = 0;
@@ -30,33 +41,51 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
     wndclass.lpszMenuName = NULL;
     wndclass.lpszClassName = szAppName;
 
-    // Регистрируем класс окна
+    
+
     if (!RegisterClass(&wndclass)) {
         MessageBox(NULL, TEXT("Ошибка регистрации класса окна!"), 
-                   TEXT("Ошибка"), MB_ICONERROR);
+                  TEXT("Ошибка"), MB_ICONERROR);
         return 0;
     }
 
-    // Создаем окно
+    // Создание главного окна
     hwnd = CreateWindow(
-        szAppName,                  // Имя класса окна
-        TEXT("Hello, WinAPI!"),     // Заголовок окна
-        WS_OVERLAPPEDWINDOW,        // Стиль окна
-        CW_USEDEFAULT,              // Позиция X
-        CW_USEDEFAULT,              // Позиция Y
-        CW_USEDEFAULT,             // Ширина
-        CW_USEDEFAULT,             // Высота
-        NULL,                      // Родительское окно
-        NULL,                      // Меню
-        hInstance,                  // Дескриптор экземпляра
-        NULL                        // Дополнительные параметры
-    );
+        szAppName,
+        TEXT("Генератор случайных булевых функций в КНФ"),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        450, 200,
+        NULL, NULL, hInstance, NULL);
 
-    // Отображаем и обновляем окно
+    // Создание статического текста (метки)
+    CreateWindow(
+        TEXT("STATIC"),
+        TEXT("Введите количество переменных функции (1-10):"),
+        WS_CHILD | WS_VISIBLE | SS_LEFT,
+        20, 20, 300, 20,
+        hwnd, (HMENU)ID_STATIC, hInstance, NULL);
+
+    // Создание поля ввода
+    CreateWindow(
+        TEXT("EDIT"),
+        TEXT(""),
+        WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER | ES_AUTOHSCROLL,
+        20, 50, 100, 25,
+        hwnd, (HMENU)ID_EDIT, hInstance, NULL);
+
+    // Создание кнопки
+    CreateWindow(
+        TEXT("BUTTON"),
+        TEXT("Сгенерировать КНФ"),
+        WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        20, 90, 150, 30,
+        hwnd, (HMENU)ID_BUTTON, hInstance, NULL);
+
     ShowWindow(hwnd, iCmdShow);
     UpdateWindow(hwnd);
 
-    // Цикл обработки сообщений
+    // Цикл сообщений
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
@@ -66,24 +95,44 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 }
 
 // Функция обработки сообщений
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    HDC hdc;
-    PAINTSTRUCT ps;
-    RECT rect;
-
-    switch (message) {
-    case WM_PAINT:
-        hdc = BeginPaint(hwnd, &ps);
-        GetClientRect(hwnd, &rect);
-        DrawText(hdc, TEXT("Hello, WinAPI!"), -1, &rect,
-            DT_SINGLELINE | DT_CENTER | DT_VCENTER);
-        EndPaint(hwnd, &ps);
-        return 0;
-
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    static TCHAR szBuffer[256];
+    
+    switch (message)
+    {
+        case WM_COMMAND:
+            if (LOWORD(wParam) == ID_BUTTON)
+            {
+                GetDlgItemText(hwnd, ID_EDIT, szBuffer, sizeof(szBuffer)/sizeof(TCHAR));
+                
+                int varCount = _wtoi(szBuffer);
+                if (varCount <= 0 || varCount > 10)
+                {
+                    MessageBox(hwnd, 
+                        TEXT("Пожалуйста, введите число от 1 до 10"), 
+                        TEXT("Некорректный ввод"), 
+                        MB_ICONWARNING);
+                }
+                else
+                {
+                    write_cnf_in_file(varCount);
+                    
+                    TCHAR messageText[100];
+                    wsprintf(messageText, 
+                        TEXT("Сгенерированы 100 КНФ с %d переменными\nРезультат сохранен в cnf_file.txt"), 
+                        varCount);
+                    MessageBox(hwnd, messageText, TEXT("Генерация завершена"), MB_OK);
+                }
+            }
+            break;
+            
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+            
+        default:
+            return DefWindowProc(hwnd, message, wParam, lParam);
     }
-
-    return DefWindowProc(hwnd, message, wParam, lParam);
+    return 0;
 }
