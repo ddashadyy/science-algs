7fca25c2879656009e97df2798ab24e96f0259c0
ddashadyy <poletaevgena75@gmail.com>
2025-04-15 23:25:21 +0800

    generation population and cnf

--- src/cnf.c
+++ src/cnf.c
@@ -5,10 +5,10 @@
 #include <stddef.h>
 #include <string.h>
 #include <math.h>
+#include <windows.h>
 
 #define AMOUNT_INDIVIDUALS 100
 
-CNF* cnfs = NULL;
 
 void init_random() 
 { 
@@ -38,108 +38,149 @@ char* generate_random_boolean_function(size_t n)
     return function;
 }
 
-char* generate_cnf_from_function(char* function) 
+char* generate_cnf(size_t num_vars, size_t num_clauses, size_t max_clause_len)
 {
-    const size_t size = strlen(function);
-    if (size == 0 || (size & (size - 1)) != 0) 
-        return NULL; 
+    char** variables = (char**)malloc(num_vars * sizeof(char*));
+    if (!variables) return NULL;
     
-    const size_t n = (size_t)log2(size);
-    if (n > 32) return NULL; 
-    
-    size_t max_cnf_len = 0;
-    size_t zero_count = 0;
-    
-    for (size_t i = 0; i < size; i++) 
-    {
-        if (function[i] == '0') 
-        {
-            zero_count++;
-            max_cnf_len += 2 + n * 4 + (n - 1) * 3;
+    for (size_t i = 0; i < num_vars; i++) {
+        variables[i] = (char*)malloc(10 * sizeof(char));
+        if (!variables[i]) 
+        {    
+            for (size_t j = 0; j < i; j++) 
+                free(variables[j]);
+            
+            free(variables);
+            return NULL;
         }
+        sprintf(variables[i], "x%zu", i + 1);
     }
     
-    if (zero_count > 1) 
-        max_cnf_len += (zero_count - 1) * 3;
-    
-    char* cnf_str = (char*)malloc(max_cnf_len + 1);
-    if (!cnf_str) return NULL;
+    size_t buf_size = 1024;
+
+    char* cnf_str = (char*)malloc(buf_size * sizeof(char));
+    if (!cnf_str) 
+    {
+        for (size_t i = 0; i < num_vars; i++) 
+            free(variables[i]);
+        
+        free(variables);
+        return NULL;
+    }
     cnf_str[0] = '\0';
     
-    size_t current_pos = 0;
-    
-    for (size_t i = 0; i < size; i++) 
-    {
-        if (function[i] == '0') 
+    for (size_t i = 0; i < num_clauses; i++) 
+    {    
+        size_t needed_space = strlen(cnf_str) + max_clause_len * 15 + 10;
+        if (needed_space >= buf_size) 
         {
-            
-            strcpy(cnf_str + current_pos, "(");
-            current_pos += 1;
-            
-            for (size_t j = 0; j < n; j++) 
+            buf_size = needed_space * 2;
+
+            char* new_buf = (char*)realloc(cnf_str, buf_size);
+            if (!new_buf) 
             {
-                if ((i & (1 << j)))
-                {
-                    strcpy(cnf_str + current_pos, "!x");
-                    current_pos += 2;
-                } 
-                else 
-                {
-                    strcpy(cnf_str + current_pos, "x");
-                    current_pos += 1;
-                }
+                free(cnf_str);
+                for (size_t j = 0; j < num_vars; j++) 
+                    free(variables[j]);
                 
-                int written = sprintf(cnf_str + current_pos, "%zu", j + 1);
-                current_pos += written;
-                
-                if (j < n - 1) 
-                {
-                    strcpy(cnf_str + current_pos, " | ");
-                    current_pos += 3;
-                }
+                free(variables);
+                return NULL;
             }
-            
-            strcpy(cnf_str + current_pos, ")");
-            current_pos += 1;
-            
-            if (i < size - 1 && strchr(function + i + 1, '0') != NULL) 
-            {
-                strcpy(cnf_str + current_pos, " & ");
-                current_pos += 3;
-            }
+            cnf_str = new_buf;
         }
+        
+        strcat(cnf_str, "(");
+        size_t clause_len = 1 + rand() % max_clause_len;
+        size_t* used_vars = (size_t*)calloc(num_vars, sizeof(size_t));
+        if (!used_vars) 
+        {
+            free(cnf_str);
+            for (size_t j = 0; j < num_vars; j++) 
+                free(variables[j]);
+            
+            free(variables);
+            return NULL;
+        }
+
+        for (size_t j = 0; j < clause_len; j++) 
+        {
+            size_t var_idx;
+            size_t attempts = 0;
+            do {
+                var_idx = rand() % num_vars;
+                attempts++;
+                
+                if (attempts > num_vars * 2) 
+                {
+                    clause_len = j;
+                    break;
+                }
+            } while (used_vars[var_idx] && clause_len > 1);
+            
+            if (j >= clause_len) continue;
+            used_vars[var_idx] = 1;
+
+            if (rand() % 3 == 0) 
+                strcat(cnf_str, "~");
+            
+            strcat(cnf_str, variables[var_idx]);
+
+            if (j < clause_len - 1) 
+                strcat(cnf_str, " | ");
+            
+        }
+        free(used_vars);
+        
+        strcat(cnf_str, ")");
+        if (i < num_clauses - 1) 
+            strcat(cnf_str, " & ");
+        
     }
     
-    return cnf_str;
+    for (size_t i = 0; i < num_vars; i++) 
+        free(variables[i]);
+    
+    free(variables);
+    
+    char* result = realloc(cnf_str, strlen(cnf_str) + 1);
+    return result ? result : cnf_str;
 }
 
 void write_cnf_in_file(size_t n)
 {
-    FILE* cnf_file = fopen("cnf_file.txt", "w+");
-    if (!cnf_file) 
+    FILE* cnf_file = fopen("cnf.txt", "w");
+    if (!cnf_file)
     {
         printf("file not created\n");
         return;
     }
 
-    cnfs = (CNF*) malloc(AMOUNT_INDIVIDUALS * sizeof(CNF));   
+    char* cnf = generate_cnf(n, n + 2, n);
+    fputs(cnf, cnf_file);
+    fputc('\n', cnf_file);
+
+    free(cnf);
+    fclose(cnf_file);
+
+}
+
+void write_candidates_in_file(size_t n)
+{
+    FILE* candidates = fopen("candidates.txt", "w+");
+    if (!candidates)
+    {
+        printf("file not created\n");
+        return;
+    }
 
     for (size_t i = 0; i < AMOUNT_INDIVIDUALS; i++)
     {
-        char* function = generate_random_boolean_function(n);
-        char* cnf = generate_cnf_from_function(function);
+        char* candidate = generate_random_boolean_function(n);
+        fputs(candidate, candidates);
+        fputc('\n', candidates);
 
-        cnfs[i].boolean_function = function;
-        cnfs[i].cnf_str = cnf;
-        
-        for (size_t i = 0; i < strlen(cnf); i++)
-        fputc(cnf[i], cnf_file);
-
-        fputc('\n', cnf_file);
-
-        free(function);
-        free(cnf);
+        free(candidate);
     }
-
-    fclose(cnf_file);
+    
+    fclose(candidates);
 }
--- src/cnf.h
+++ src/cnf.h
@@ -5,19 +5,14 @@
 #include <inttypes.h>
 #include <stdio.h>
 
-// пока что одна из идей это создать массив на 100 кнф по этой структуре 
-// и работать с ней уже при реализации алгоритмов
-typedef struct CNF_t {
-    char* cnf_str;
-    char* boolean_function;
-} CNF;
-
-extern CNF* cnfs;
 
 void init_random(); 
+
 char* generate_random_boolean_function(size_t n);
-char* generate_cnf_from_function(char* function);
+char* generate_cnf(size_t num_vars, size_t num_clauses, size_t max_clause_len);
+
 void write_cnf_in_file(size_t n);
+void write_candidates_in_file(size_t n);
 
 
-#endif
+#endif // CNF_H
