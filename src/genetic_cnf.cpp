55c905914c9c65ea439100e105009b2572090df1
ddashadyy <poletaevgena75@gmail.com>
2025-04-21 01:21:19 +0800

    теперь выполнение алгоритма не генерирует заново КНФ и кандидатов

--- src/genetic_cnf.cpp
+++ src/genetic_cnf.cpp
@@ -8,39 +8,35 @@
 constexpr double EPSILON = std::numeric_limits<double>::epsilon() * 10;
 
 GeneticAlgorithm::GeneticAlgorithm(
+    const CNF &cnf,
+    const Candidates &candidates,
+    selection_function sf) : _cnf(cnf), _candidates(candidates), _sf(sf) {}
+
+GeneticAlgorithm::~GeneticAlgorithm() { std::cout << "GeneticAlgorithm destroyed\n"; }
+
+std::pair<std::size_t, std::string> GeneticAlgorithm::execute(
     std::size_t iterations,
-    std::size_t function_length,
-    std::size_t population, 
-    std::size_t hybridizations, 
-    std::size_t mutations, 
-    std::size_t amount_gens_mutations, 
-    selection_function sf
-) : _iterations(iterations), _function_length(function_length) ,_population(population), _hybridizations(hybridizations), 
-    _mutations(mutations), _amount_gens_mutation(amount_gens_mutations), _sf(sf)
-{
-    this->_cnf = CNF(function_length);
-    this->_candidates = Candidates(population, function_length);
-}
-
-GeneticAlgorithm::~GeneticAlgorithm() = default;
-
-std::pair<std::size_t, std::string> GeneticAlgorithm::execute() 
+    std::size_t population,
+    std::size_t hybridizations,
+    std::size_t mutations,
+    std::size_t amount_gens_mutations
+)
 {
     auto& candidates = this->_candidates.get_candidates();
     std::size_t iteration_counter = 1;
 
-    for (std::size_t i = 0; i < this->_iterations; i++)
-    {    
+    for (std::size_t i = 0; i < iterations; i++)
+    {
         for (auto& candidate : candidates)
         {
             candidate.evaluate_quality_function(this->_cnf);
-            if (std::fabs(candidate.get_quality() - 1.0) < EPSILON) 
+            if (std::fabs(candidate.get_quality() - 1.0) < EPSILON)
                 return std::make_pair(iteration_counter, candidate.get_function());
-            
         }
-        hybridizate(candidates);
-        mutate(candidates);
-        do_selection(candidates);
+
+        hybridizate(candidates, hybridizations, population);
+        mutate(candidates, population, mutations, amount_gens_mutations);
+        do_selection(candidates, population);
 
         ++iteration_counter;
     }
@@ -48,13 +44,17 @@ std::pair<std::size_t, std::string> GeneticAlgorithm::execute()
     return std::make_pair(iteration_counter, std::string{"there is no solution"});
 }
 
-void GeneticAlgorithm::hybridizate(std::vector<Candidate>& candidates)
+void GeneticAlgorithm::hybridizate(
+    std::vector<Candidate>& candidates,
+    std::size_t hybridizations,
+    std::size_t population
+)
 {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<std::size_t> distrib(0, this->_candidates.get_candidates()[0].get_function().size() - 1);
 
-    for (std::size_t i = 0; i < this->_hybridizations; i += 2)
+    for (std::size_t i = 0; i < hybridizations; i += 2)
     {
         std::size_t first_parent_index = 0;
         std::size_t second_parent_index = 0;
@@ -66,36 +66,41 @@ void GeneticAlgorithm::hybridizate(std::vector<Candidate>& candidates)
             break;
 
         case selection_function::LINEAR:
-            first_parent_index = i % (this->_population - 1);
-            second_parent_index = i % (this->_population - 1);
+            first_parent_index = i % (population - 1);
+            second_parent_index = i % (population - 1);
             break;
 
         case selection_function::EXPONENTIAL:
-            first_parent_index = (i * i) % (this->_population - 1);
-            second_parent_index = (i * i) % (this->_population - 1);
+            first_parent_index = (i * i) % (population - 1);
+            second_parent_index = (i * i) % (population - 1);
             break;
-        
+
         default:
             break;
         }
 
         std::size_t point = distrib(gen);
 
-        std::string child = 
-            candidates[first_parent_index].get_function().substr(0, point) + 
+        std::string child =
+            candidates[first_parent_index].get_function().substr(0, point) +
             candidates[second_parent_index].get_function().substr(point);
 
         candidates.emplace_back(child);
     }
 }
 
-void GeneticAlgorithm::mutate(std::vector<Candidate>& candidates)
+void GeneticAlgorithm::mutate(
+    std::vector<Candidate>& candidates,
+    std::size_t population,
+    std::size_t mutations,
+    std::size_t amount_gens_mutations
+)
 {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<std::size_t> distrib(0, this->_population - 1);
+    std::uniform_int_distribution<std::size_t> distrib(0, population - 1);
 
-    for (std::size_t i = 0; i < this->_mutations; i++)
+    for (std::size_t i = 0; i < mutations; i++)
     {
         std::size_t candidate_index_to_mutate = 0;
 
@@ -106,34 +111,35 @@ void GeneticAlgorithm::mutate(std::vector<Candidate>& candidates)
             break;
 
         case selection_function::LINEAR:
-            candidate_index_to_mutate = i % (this->_population - 1);
+            candidate_index_to_mutate = i % (population - 1);
             break;
 
         case selection_function::EXPONENTIAL:
-            candidate_index_to_mutate = (i * i) % (this->_population - 1);
+            candidate_index_to_mutate = (i * i) % (population - 1);
             break;
-        
+
         default:
             break;
         }
 
-        for (std::size_t j = 0; j < this->_amount_gens_mutation; j++)
+        for (std::size_t j = 0; j < amount_gens_mutations; j++)
         {
             std::uniform_int_distribution<std::size_t> distrib(0, candidates[i].get_function().size() - 1);
             std::size_t mutation_point = distrib(gen);
 
-            candidates[candidate_index_to_mutate].get_function()[mutation_point] = 
+            candidates[candidate_index_to_mutate].get_function()[mutation_point] =
                 (candidates[candidate_index_to_mutate].get_function()[mutation_point] == '0') ? '1' : '0';
         }
     }
-   
 }
 
-void GeneticAlgorithm::do_selection(std::vector<Candidate> &candidates)
+void GeneticAlgorithm::do_selection(
+    std::vector<Candidate> &candidates,
+    std::size_t population
+)
 {
-    std::sort(candidates.begin(), candidates.end(), [](const Candidate& lhs, const Candidate& rhs){
-        return lhs.get_quality() < rhs.get_quality();
-    });
+    std::sort(candidates.begin(), candidates.end(), [](const Candidate &lhs, const Candidate &rhs)
+              { return lhs.get_quality() < rhs.get_quality(); });
 
-    candidates.resize(this->_population);
+    candidates.resize(population);
 }
--- src/genetic_cnf.hpp
+++ src/genetic_cnf.hpp
@@ -3,42 +3,44 @@
 
 #include "cnf.hpp"
 #include <utility>
-    
 
 class GeneticAlgorithm
 {
 public:
-
     GeneticAlgorithm(
-        std::size_t iterations,
-        std::size_t function_length,
-        std::size_t population, 
-        std::size_t hybridizations,
-        std::size_t mutations,
-        std::size_t amount_gens_mutations,
-        selection_function sf
-    );
+        const CNF& cnf,
+        const Candidates& candidates,
+        selection_function sf);
     ~GeneticAlgorithm();
 
-   std::pair<std::size_t, std::string> execute();
+    std::pair<std::size_t, std::string> execute(
+        std::size_t iterations,
+        std::size_t population,
+        std::size_t hybridizations,
+        std::size_t mutations,
+        std::size_t amount_gens_mutations
+    );
 
 private:
+    void hybridizate(
+        std::vector<Candidate>& candidates,
+        std::size_t hybridizations,
+        std::size_t population
+    );
+    void mutate(
+        std::vector<Candidate>& candidates,
+        std::size_t population,
+        std::size_t mutations,
+        std::size_t amount_gens_mutations
+    );
+    void do_selection(
+        std::vector<Candidate>& candidates,
+        std::size_t population
+    );
 
-    void hybridizate(std::vector<Candidate>& candidates);
-    void mutate(std::vector<Candidate>& candidates);
-    void do_selection(std::vector<Candidate>& candidates);
-
-    CNF _cnf{};
-    Candidates _candidates{}; 
-
-    std::size_t _iterations;
-    std::size_t _function_length;
-    std::size_t _population;
-    std::size_t _hybridizations;
-    std::size_t _mutations;
-    std::size_t _amount_gens_mutation;
+    CNF _cnf;
+    Candidates _candidates;
     selection_function _sf;
 };
-
 
 #endif // GENETIC_CHF_HPP
